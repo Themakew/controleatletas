ddc040ac69f0d89729f9d89fa4f2eeae8a2e83ff
Ruyther <ruyther@ubuntu.(none)>
2013-05-16 23:35:32 -0300

    Add classes

---
+++ ControleAtleta/src/util/constantes.java
@@ -0,0 +1,42 @@
+package controleatleta.util;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Ruyther
+ */
+public class constantes {
+    public static final byte SEXO_MASCULINO_INDICE = 0;
+    public static final byte SEXO_FEMININO_INDICE = 1;
+    public static final String SEXO_MASCULINO_VALOR = "M";
+    public static final String SEXO_FEMININO_VALOR = "F";
+    public static final byte CATEGORIA_MIRIM_ASPIRANTE_INDICE = 0;
+    public static final byte CATEGORIA_INFANTIL_ASPIRANTE_INDICE = 1;
+    public static final byte CATEGORIA_INFANTOJUVENIL_ASPIRANTE_INDICE=2;
+    public static final byte CATEGORIA_PREJUVENIL_ASPIRANTE_INDICE = 3;
+    public static final byte CATEGORIA_JUVENIL_ASPIRANTE_INDICE = 4;
+    public static final byte CATEGORIA_ADULTO_ASPIRANTE_INDICE = 5;
+    public static final byte CATEGORIA_PREJUVENIL_ESPECIAL_INDICE = 6;
+    public static final byte CATEGORIA_JUVENIL_ESPECIAL_INDICE = 7;
+    public static final byte CATEGORIA_JUNIOR_ESPECIAL_INDICE = 8;
+    public static final byte CATEGORIA_SENIOR_ESPECIAL_INDICE = 9;
+    public static final String CATEGORIA_MIRIM_ASPIRANTE_VALOR = "Mirim Aspirante";
+    public static final String CATEGORIA_INFANTIL_ASPIRANTE_VALOR = "Infantil Aspirante";
+    public static final String CATEGORIA_INFANTOJUVENIL_ASPIRANTE_VALOR = "Infanto Juvenil Aspirante";
+    public static final String CATEGORIA_PREJUVENIL_ASPIRANTE_VALOR = "Pre Juvenil Aspirante";
+    public static final String CATEGORIA_JUVENIL_ASPIRANTE_VALOR = "Juvenil Aspirante";
+    public static final String CATEGORIA_ADULTO_ASPIRANTE_VALOR = "Adulto Aspirante";
+    public static final String CATEGORIA_PREJUVENIL_ESPECIAL_VALOR = "Pre Juvenil Especial";
+    public static final String CATEGORIA_JUVENIL_ESPECIAL_VALOR = "Juvenil Especial";
+    public static final String CATEGORIA_JUNIOR_ESPECIAL_VALOR = "Junior Especial";
+    public static final String CATEGORIA_SENIOR_ESPECIAL_VALOR = "Senior Especial";
+    public static final byte ESTILO_ORTODOXO_INDICE = 0;
+    public static final byte ESTILO_SOUTHPAW_INDICE = 1;
+    public static final char ESTILO_ORTODOXO_VALOR = 'O';
+    public static final char ESTILO_SOUTHPAW_VALOR = 'S';
+}
---
+++ ControleAtleta/test/model/AtletaTest.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class AtletaTest {
+    
+    @Test public void getNome(){
+       Assert.assertEquals("nome", "nome");
+    }
+}
